118e8bc08d0c3e37642003c37451e03af75102a8
John Idso <john.d.idso@gmail.com>
2021-06-19 11:57:46 -0500

    add, display, and delete working

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-$( document ).ready(onPageLoad);
+$( document ).ready(onReady);
 
-function onPageLoad(){
-    $("#submitButton").on("click", createRecord
-    )
+function onReady(){
+    $("#submitButton").on("click", createRecord);
+    $(document).on("click", '.delete', function(){
+        $(this).closest('tr').remove();
+    })
 }
 
 /* 
@@ -11,14 +13,38 @@ submit button press
 delete button press
 */ 
 
-// get inputs and place them in DOM 
+// get inputs, place them in DOM, clear inputs
+function createRecord(){
+    let firstName = $("#firstNameIn").val();
+    let lastName = $("#lastNameIn").val();
+    let employeeID = $("#idIn").val();
+    let employeeTitle = $("#titleIn").val();
+    let annualSalary = $("#annualSalaryIn").val();
+    $("#tableRows").append(
+        `<tr id="row${employeeID}">
+            <td>${firstName}</td>
+            <td>${lastName}</td>
+            <td>${employeeID}</td>
+            <td>${employeeTitle}</td>
+            <td>${annualSalary}</td>
+            <td class="delete"><button type="button" class="btn btn-danger" id="delete${employeeID}">Delete</button></td>
+        </tr>`
+        )
+    clearInputs();
+} // using employee ID for delete ID as value should be unique
 
+function clearInputs(){
+    $("#firstNameIn").val('');
+    $("#lastNameIn").val('');
+    $("#idIn").val('');
+    $("#titleIn").val('');
+    $("#annualSalaryIn").val('');
+}
 
 // adjust monthly cost on button click 
 
 
 // add function to delete user 
-
 
 // handle total monthly cost > $20,000 (.addClass Danger)
 
