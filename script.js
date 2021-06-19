dee33584f7bc65e36714c4018caa1d91eb5e8895
John Idso <john.d.idso@gmail.com>
2021-06-19 10:38:46 -0500

    scoping out script needs

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 $( document ).ready(onPageLoad);
 
 function onPageLoad(){
-    
+    $("#submitButton").on("click", createRecord
+    )
 }
+
+/* 
+listeners needed - 
+submit button press 
+delete button press
+*/ 
+
+// get inputs and place them in DOM 
+
+
+// adjust monthly cost on button click 
+
+
+// add function to delete user 
+
+
+// handle total monthly cost > $20,000 (.addClass Danger)
+
+
+
+
