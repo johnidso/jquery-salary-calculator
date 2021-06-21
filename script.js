65fb84704611fd23079fa835635be9e487d51e81
John Idso <john.d.idso@gmail.com>
2021-06-21 11:24:10 -0500

    adjusted to report monthly total vs annual total salary

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ function createRecord(){
                 <td class="delete"><button type="button" class="btn btn-danger" id="${employeeID}Delete">Delete Record</button></td>
             </tr>`
             ) // *** For the purposes of this assignment I am assuming that employee IDs are UNIQUE ***
-        $(`#row${employeeID}`).data("salary", annualSalary); // assign salary amount into table row metadata
+        $(`#row${employeeID}`).data("salary", parseInt(annualSalary/12)); // assign salary amount into table row metadata
         adjustMonthly(annualSalary);
         clearInputs();
     }
@@ -62,7 +62,7 @@ function clearInputs(){
 
 // adjust monthly cost only if EXCEEDS (>) $20,000
 function adjustMonthly(salaryValue){
-    totalMonthlySalary += parseInt(salaryValue);
+    totalMonthlySalary += parseInt(salaryValue/12);
     $("#totalMonthly").text(`$${totalMonthlySalary}`);
     if(totalMonthlySalary > 20000){
         $("#totalMonthly").removeClass();
